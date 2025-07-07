9196f0560c5a5fbe62f2720bffb0f23b2e9caa77
Safeedit <safeeditpdf@gmail.com>
2025-07-07 15:45:10 +0530

    Update app.py

--- image-edit-service/app.py
+++ image-edit-service/app.py
@@ -128,4 +128,5 @@ def enhance():
         return jsonify({"error": "Request failed with status code 500: " + str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
